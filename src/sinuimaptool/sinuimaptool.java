b3327be9d2a489f26ca0f43427729f5ae1334723
Sinutok <1307github@gmx.de>
2017-01-18 16:38:21 +0100

    Initial Eclipse

---
+++ src/sinuimaptool/sinuimaptool.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package sinuimaptool;
+
+/**
+ * @author Quwartos
+ *
+ */
+public class sinuimaptool
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
